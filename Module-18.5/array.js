c537ed19266f7fe6c5da901eebb50388b95c093d
mahmud2237 <smahmud2237@gmail.com>
2024-11-04 21:12:28 +0600

    Javascript code added

---
+++ Module-18.5/array.js
@@ -0,0 +1,9 @@
+const fruits =["apple", "banana", "lichi"];
+fruits[0] = "Mango";
+console.log("After indexing", fruits);
+
+fruits.push("orange");
+console.log("After Push", fruits);
+
+fruits.pop();
+console.log("After pop", fruits);
