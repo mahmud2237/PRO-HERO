ffc27d4c14d589eaacaaabf26fdd9c8426f7ade8
mahmud2237 <smahmud2237@gmail.com>
2024-12-06 00:06:53 +0600

    Some examples of DOM

---
+++ Module-25/js/script.js
@@ -0,0 +1,10 @@
+const allHeadings = document.getElementsByTagName('h1');
+for(const h1 of allHeadings){
+    console.log(h1.innerText);
+}
+
+const liCollection = document.getElementsByTagName('li');
+// console.log(liCollection);
+for(const li of liCollection){
+    console.log(li.innerHTML);
+}
