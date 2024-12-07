891283bff63ea76a3ad8c0787fe3f998e344a23c
mahmud2237 <smahmud2237@gmail.com>
2024-12-07 19:01:58 +0600

    Final DOM Event Handlers

--- Module-26/js/dom1.js
+++ Module-26/js/dom1.js
@@ -1,1 +1,19 @@
-console.log('Special Dom Events1')
+function handleOnClick(){
+    const handlerStatus = document.getElementById('handler-status');
+    handlerStatus.innerText = 'Handled by function attached on onclick attribute';
+}
+
+document.getElementById('event-listener').addEventListener('click', function(){
+    const handlerStatus = document.getElementById('handler-status');
+    handlerStatus.innerText = 'Handled by event listener';
+})
+
+document.getElementById('btn-update').addEventListener('click', function(){
+    const inputField = document.getElementById('input-text');
+    // console.log(inputText.value);
+    const inputTextValue = inputField.value;
+
+    const p = document.getElementById('input-display');
+    p.innerText = inputTextValue;
+    inputField.value = '';
+})
