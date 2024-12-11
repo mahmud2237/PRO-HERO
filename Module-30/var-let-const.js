402a18fac52fb5d273248193a44b2fd51e441d4a
mahmud2237 <smahmud2237@gmail.com>
2024-12-11 14:19:24 +0600

    Var-Let-Const ES6 Recap

--- Module-30/var-let-const.js
+++ Module-30/var-let-const.js
@@ -14,9 +14,18 @@ count = count + 15;
 console.log(count);
 
 const numbers = [23, 15, 65, 45, 32];
+// numbers = [12, 34, 53] // This will throw an error. Because we can't change value with const variable.
 numbers[1] = 55;
 numbers.unshift(8, 9); // This will not throw an error. Because we can't change array elements.
 numbers.push(37 , 50); // This will not throw an error. Because we can't change array elements.
 console.log(numbers);
 
 
+//for object // This is also same. if we use const than it will throw error if we want to reassign vaiable.
+const student = {
+    name: 'John',
+    age: 25,
+    city: 'New York'
+}
+
+// if we need to reassign value in the variable than we need to use let instead of const.
