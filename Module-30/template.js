c159e4c15e1c7c3ddfec59a5c4d6d00ec730b25d
mahmud2237 <smahmud2237@gmail.com>
2024-12-11 14:50:56 +0600

    Template Backtick in ES6

---
+++ Module-30/template.js
@@ -0,0 +1,18 @@
+const first = 'Jaan';
+const last = 'pakhi';
+const greet = 'potas potas?'
+const name = first + ' ' + last + " " + greet;
+console.log(name);
+
+const a = 10;
+const b = 20;
+// const result = 'The sum of' + a + ' and ' + b + ' is ' + (a+b); // This is so difficult
+const result = `The sum of ${a} and ${b} is ${a + b}`; // This easy and readable
+console.log(result);
+
+const templete = `Hey there.
+How are you?
+Are you local here?
+What's your name?`;
+
+console.log(templete);
