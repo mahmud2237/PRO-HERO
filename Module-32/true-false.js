833548e1826607e2ffe8b3db2b0230f15393b4ae
mahmud2237 <smahmud2237@gmail.com>
2024-12-13 16:10:05 +0600

    True-false with example

---
+++ Module-32/true-false.js
@@ -0,0 +1,27 @@
+/**
+    Truthy:
+    1. true
+    2. any number (+ve or -ve) except 0
+    3. Any string other than empty string ('')
+    4. '0' or 'false' like this all is string and truthy
+    5. {empty object} and Object 
+    6. [empty array] and Array
+    7. !!value (double bang! bang!)
+
+    Falsy:
+    1. false
+    2. 0 
+    3. empty string ('')
+    4. undefined
+    5. null
+    6. Nan
+    7. !value (single bang!)
+ */
+
+let x = NaN;
+console.log(x);
+if(x){
+    console.log('value of x is truthy');
+}else{
+    console.log('value of x is falsy');
+}
