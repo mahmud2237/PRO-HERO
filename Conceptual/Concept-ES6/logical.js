86bb913acfa3fd30d9c4fd903c848896646875a8
mahmud2237 <smahmud2237@gmail.com>
2024-12-15 19:52:52 +0600

    Logical and ternary operator Examples

---
+++ Conceptual/Concept-ES6/logical.js
@@ -0,0 +1,27 @@
+
+// logical operator (and, or, not) && || !
+// const x = 5;
+//console.log(!!x);  
+
+
+const money = true;
+// and && will work if left side work than only work right side
+// left && right
+// console.log(money && 'varsity jabo');
+
+
+// in or if left side worked than right side won't. 
+//or ||
+// const bus = 'bus ache';
+// const rickshaw = 'rickshaw ase';
+// console.log(bus || rickshaw);
+
+
+// ternary operator //From now we will always work with this ternary operator
+// statement ? if true : false;
+const x = 5;
+console.log(x === 5? 'number is 5' : 'not five'); 
+
+
+
+
