1a1f1c6e9ea278c426742eed69acc108880ec727
mahmud2237 <smahmud2237@gmail.com>
2024-12-22 23:15:04 +0600

    ForEach with Examples

---
+++ Conceptual/Concept-ES6/forEach.js
@@ -0,0 +1,31 @@
+const arr = [10, 20, 30, 40, 50];
+
+// for(let item of arr){
+//     console.log(item);
+// }
+// we can do this iteration using forEach or map
+// forEach and map work as like the similar way  // arr.forEach((item) => item) // arr.map()
+
+
+const show = (a) => console.log(a);
+arr.forEach(show);
+
+// Or same thing using direct forEach
+arr.forEach((a) => {
+    console.log(a);
+}) 
+
+// if I want to get total from the given arr
+let sum = 0;
+let total = arr.forEach((item) => {
+    sum += item;
+})
+console.log(sum);
+
+// callback parameters (item, index, whole array) // always first one will give item/keys than index and last one will give us the whole array
+arr.forEach((item, index, whole) => {
+    console.log(`index: ${index} item: ${item} and the whole Arr: ${whole}`);
+});
+
+
+
