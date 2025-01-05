cc39ebe95a50551c5d50dd5650130a54d3afa41c
mahmud2237 <smahmud2237@gmail.com>
2025-01-05 15:57:12 +0600

    Counter.jsx with some handler buttons(add, reduce)

--- Module-39/explore-react-core-concepts-part2/src/App.jsx
+++ Module-39/explore-react-core-concepts-part2/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Counter from './Counter';
 
 function App() {
 
@@ -10,12 +11,19 @@ function App() {
     alert('button 2 clicked');
   }
 
+  const addToFive = (num) => {
+    num = num + 5;
+    alert(num);
+  }
+
   return (
     <>
       <h3>React Core Concepts 2</h3>
+      <Counter ></Counter>
       <button onClick={handleClick}>Btn 1</button>
       <button onClick={handleClick2}>Btn 2</button>
       <button onClick={() => {alert('button 3 clicked')}}>Btn 3</button>
+      <button onClick={() => addToFive(3)}>Btn 4</button>
     </>
   )
 }
