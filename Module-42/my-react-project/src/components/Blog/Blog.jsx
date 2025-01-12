610c02e595ce7d17452ae4fece0a3dd6cf90872d
mahmud2237 <smahmud2237@gmail.com>
2025-01-13 00:06:23 +0600

    bookmark icon added

--- Module-42/my-react-project/src/components/Blog/Blog.jsx
+++ Module-42/my-react-project/src/components/Blog/Blog.jsx
@@ -1,16 +1,40 @@
 import PropTypes from 'prop-types';
+import { IoBookmarksOutline } from 'react-icons/io5';
 
 const Blog = ({blog}) => {
+    const { title, cover, author,
+         author_img, reading_time, 
+         posted_date, hashtags } = blog;
+
     console.log(blog);
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`cover pic of the ${title}`} />
+            <div className='flex flex-row justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>Posted: {posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} mins read</span>
+                    <button className='ml-2 text-2xl text-red-600'><IoBookmarksOutline /></button>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href=""> #{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired;
+    blog: PropTypes.object.isRequired,
 }
 
 export default Blog;
--- Module-42/my-react-project/src/components/Blogs/Blogs.jsx
+++ Module-42/my-react-project/src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 
 const Blogs = () => {
@@ -10,8 +11,15 @@ const Blogs = () => {
         .then(data => setBlogs(data))
     }, [])
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog 
+                    key={blog.id} 
+                    blog={blog}
+                    
+                    ></Blog>)
+            }
         </div>
     );
 };
